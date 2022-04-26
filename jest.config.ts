8cee1d2e049ce6f44d1aee234d8be99746719918
Marcio Dauri <mdauri@gmail.com>
2022-04-25 21:06:44 -0300

    jest fix

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,3 @@
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
 import { pathsToModuleNameMapper } from "ts-jest";
 
 import { compilerOptions } from "./tsconfig.json";
